2f61b339b05fec0c332782c9e5d338edfd4485ed
Daniel Aviv <76743397+o3wiz@users.noreply.github.com>
2026-06-19 17:51:16 +0300

    fixed QColor assignment of main, and quick find background colors (#2)

--- src/settings/src/configuration.cpp
+++ src/settings/src/configuration.cpp
@@ -145,7 +145,7 @@ void Configuration::retrieveFromStorage( QSettings& settings )
 #if QT_VERSION <= QT_VERSION_CHECK( 6, 4, 0 )
         .setNamedColor(
 #else
-        .fromString(
+        = QColor::fromString(
 #endif
             settings
                 .value( "regexpType.mainBackColor",
@@ -156,7 +156,7 @@ void Configuration::retrieveFromStorage( QSettings& settings )
 #if QT_VERSION <= QT_VERSION_CHECK( 6, 4, 0 )
         .setNamedColor(
 #else
-        .fromString(
+        = QColor::fromString(
 #endif
             settings
                 .value( "regexpType.quickfindBackColor",
